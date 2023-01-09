64f6036a6446892145781ebbee1aefdbebb1349e
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2023-01-09 12:07:11 -0500

    init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreWebApplicationMVC7Template.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
